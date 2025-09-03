51d2994c26b4d6bdd4f1c0b2a16a7a9831a36702
Raj Agrahari <raj.agr2002@gmail.com>
2025-09-03 23:42:25 +0530

    Adding

--- JS/addThreeNum.js
+++
@@ -1,7 +0,0 @@
-let a  = 20;
-let b  = 10;
-let c  = 60;
-
-let sum  = a+b+c;
-console.log("The sum of three is: " + sum);
-
--- JS/addingTwoNum.js
+++
@@ -1,5 +0,0 @@
-let a = 5;
-let b = 10;
-
-let sum = a + b;
-console.log("The sum is: " + sum);
