dfca6eddc0151b7eb30fa4c02714da1ea555ec02
Raj Agrahari <raj.agr2002@gmail.com>
2025-09-06 22:07:35 +0530

    Adding

---
+++ JS (03.09.25)/Day 4 (Data type)/datatype.js
@@ -0,0 +1,32 @@
+// primitive type datatype
+
+let a =10;
+console.log(typeof a);
+ a =10.5;
+console.log(typeof a);
+ a ='abc';
+console.log(typeof a);
+ a =false;
+ console.log(typeof a);
+ a =undefined;
+console.log(typeof a);
+ a =null;
+console.log(typeof a);
+ a =1n;
+console.log(typeof a);
+a = Symbol('Raj')
+console.log(typeof a);
+
+// non primitive data
+
+let b= [10, 20];
+console.log(typeof b);
+b= {};
+console.log(typeof b);
+ b= new Date();
+console.log(typeof b);
+console.log(typeof Math);
+
+
+
+
