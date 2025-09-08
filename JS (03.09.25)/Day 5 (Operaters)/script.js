4f5d651c011cdbc8f32744db90a1393f2167f3c0
Raj Agrahari <raj.agr2002@gmail.com>
2025-09-08 23:06:05 +0530

    Adding

---
+++ JS (03.09.25)/Day 5 (Operaters)/script.js
@@ -0,0 +1,25 @@
+// let name = prompt("Enter your name")
+// console.log(name);
+
+// console.log(10 && 20);
+// console.log(0 && 20);
+// console.log(10 && 0);
+
+
+// let username = prompt("enter your username");
+
+
+// console.log('Hello', username || 'guest')
+// // console.log('Hello', guest)raj
+
+// let num1 = parseInt(prompt("Enter the num 1"));
+// let num2 = parseInt(prompt("Enter the num 2"));
+
+// console.log(`the num one is ${num1} and num2 is ${num2} and sum of them is ${num1 + num2}`);
+
+
+
+
+
+
+
---
+++ JS (03.09.25)/Day 5 (Operaters)/taskOne.js
@@ -0,0 +1,11 @@
+
+
+ 
+
+let age = parseInt(prompt("Enter the age"));
+
+
+console.log(`${age<18} I would like to have a glass of milk` || ` ${age>18} I would like to have a glass of something`);
+
+
+// let age = parseInt(prompt("Enter the age"));
