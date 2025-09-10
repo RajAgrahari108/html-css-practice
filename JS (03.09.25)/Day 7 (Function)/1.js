d8c57136e4abd9bf2c992683f6128245f6bb9242
Raj Agrahari <raj.agr2002@gmail.com>
2025-09-10 22:30:50 +0530

    Adding

---
+++ JS (03.09.25)/Day 7 (Function)/1.js
@@ -0,0 +1,23 @@
+// function sum (a, b){
+//     console.log(a+b);
+// }
+// sum(2, 25)
+
+
+
+var a = 10;
+let b = 20;
+const c= 30;
+{
+    var a = 10;
+    let b = 20;
+    const c= 30;
+
+}
+function fun() {
+    var a = 10;
+    let b = 20;
+    const c= 30;
+
+}
+fun();
