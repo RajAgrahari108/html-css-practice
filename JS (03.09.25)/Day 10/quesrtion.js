2b171f3a3257c903954381f1aaa3b6256625b457
Raj Agrahari <raj.agr2002@gmail.com>
2025-09-15 23:33:05 +0530

    Adding

---
+++ JS (03.09.25)/Day 10/quesrtion.js
@@ -0,0 +1,25 @@
+// function countVowels(str) {
+//   let vowels = "aeiouRAJAGRAHARI";
+//   let count = 0;
+
+//   for (let i = 0; i < str.length; i++) {
+//     if (vowels.includes(str[i])) {
+//       count++;
+//     }
+//   }
+//    return count;
+// }
+
+// let text = "rajagrahari";
+// console.log("Vowels:", countVowels(text));
+
+
+
+
+
+  let text = "Hello   world   this is   JavaScript";
+  let words = text.split('');
+    console.log(words.length);
+
+
+
---
+++ JS (03.09.25)/Day 10/split.js
@@ -0,0 +1,12 @@
+// let str = 'This is a javascript';
+// console.log(str.split());
+// console.log(str.split(' '));
+// console.log(str.split(''));
+
+
+// let str2 = 'a javascript';
+// console.log(str2.split('a'));
+
+
+
+
