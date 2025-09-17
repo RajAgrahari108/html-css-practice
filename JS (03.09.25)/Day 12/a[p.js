a3dff4c4ce67cba2ab094bdaf470a3ea6beb5de5
Raj Agrahari <raj.agr2002@gmail.com>
2025-09-17 19:35:26 +0530

    Adding

--- JS (03.09.25)/Day 12/a[p.js
+++ JS (03.09.25)/Day 12/a[p.js
@@ -7,4 +7,18 @@
 
 
 
+// let name = ['raj', 'atul', 'gupta'];
+// let res = name.filter((el, i)=>{
+//     return el.charAt(0).includes('a', 'e', 'i','o','u');
+// })
+// console.log(res);
+
+
+
+// let sal = [1000, 2000, 500, 30000, 70000, 100000];
+// let resi = sal.map((el)=>{
+//     return el + el*0.1;
+// })
+// console.log(res);
+
 
