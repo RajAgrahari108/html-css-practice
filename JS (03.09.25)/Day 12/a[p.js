33a3195832028fda269173254eb7cc615fbc55cd
Raj Agrahari <raj.agr2002@gmail.com>
2025-09-17 19:34:23 +0530

    Adding

---
+++ JS (03.09.25)/Day 12/a[p.js
@@ -0,0 +1,10 @@
+// let name = ['raj', 'atul', 'gupta'];
+// let res = name.map((el)=>{
+//     return el.charAt(0).toUpperCase()+el.slice(1).toLowerCase();
+// })
+
+// console.log(res);
+
+
+
+
---
+++ JS (03.09.25)/Day 12/tempCodeRunnerFile.js
@@ -0,0 +1,5 @@
+let filter = ['index.html', 'script.js', 'docs.dock', 'demo.txt', 'hello.java', 'style.css']
+ 
+// let snd = filter.map((el, i)=>{
+//     return el.endsWith('html') || el.endsWith('css') ||el.endsWith('js') ||el.endsWith('java');
+// })
