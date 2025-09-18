59f2b1d7f404d23e22cde858c41ca3b2327bdf12
Raj Agrahari <raj.agr2002@gmail.com>
2025-09-18 11:19:42 +0530

    Adding

---
+++ JS (03.09.25)/Day 13/index.js
@@ -0,0 +1,39 @@
+// //! reduce 
+
+// let arr = [10,20, 30, 40];
+// let sum = arr.reduce((acc, el, i, arr)=>{
+//     return acc + el;
+// })
+
+// let prod = arr.reduce((acc, el, i, arr)=>{
+//     return acc * el;
+// })
+
+// let temp = arr.reduce((acc, el, i, arr)=>{
+//     console.log(acc, el);
+    
+//     return acc + el;
+// })
+// // ! after passing the initalvalue
+// let summ = arr.reduce((acc, el, i, arr)=>{
+//      console.log(acc, el);
+//     return acc + el;
+// },500)
+
+// console.log(sum);
+// console.log(prod);
+// console.log(temp);
+// console.log(summ);
+
+// console.log("======================================================================")
+// // ! reduceRight
+
+// let summm = arr.reduceRight((acc, el, i, arr)=>{
+//      console.log(acc, el);
+//     return acc + el;
+// },500)
+// console.log(summm);
+
+
+
+
