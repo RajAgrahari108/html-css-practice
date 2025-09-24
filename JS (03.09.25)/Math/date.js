7b15305e447022310edd9fbd060c7731a934b0ca
Raj Agrahari <raj.agr2002@gmail.com>
2025-09-24 23:46:29 +0530

    Adding

---
+++ JS (03.09.25)/Math/date.js
@@ -0,0 +1,10 @@
+function gertMonth(){
+    let arr = ["jan", "feb", "mar", "april", "may", "june", "july", "aug", "sept", "oct", "nov", "dec" ];
+    console.log(arr[1]);
+}
+
+
+
+
+let arrDay = ["SUNDAY", "MONDAY", "TUESDAY", "WEDNESDAY", "THRUSDAY", "FRIDAY", "SATURDAY"];
+console.log(arr[1]);
---
+++ JS (03.09.25)/Math/math.js
@@ -0,0 +1,35 @@
+// function generateOPT(){
+    
+// let random = Math.round(Math.random()*8999)+1000;
+
+// console.log(random);
+// let opt = prompt("Enter the otp");
+// // console.log(otp);
+
+
+
+
+// if(opt==random){
+//     console.log("your loging");
+    
+// }else{
+//     console.log("Wrong otp");
+    
+// }
+
+// }
+// generateOPT()
+
+
+
+// ! generate a random color
+
+
+// function randimColor()
+// {
+//     let r = Math.round(Math.random()*255);
+//     let g = Math.round(Math.random()*255);
+//     let b = Math.round(Math.random()*255);
+//     return `rgb(${r}, ${g}, ${b} )`
+// }
+// console.log(randimColor());
