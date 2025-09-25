6e04aa059dec8e4681ad455b38715da698c8dd77
Raj Agrahari <raj.agr2002@gmail.com>
2025-09-25 11:07:51 +0530

    Adding

---
+++ JS (03.09.25)/Day 15(BOM)/app.js
@@ -0,0 +1,16 @@
+// ! navigator
+
+// console.log(navigator.onLine);
+
+
+//! location
+
+console.log(location);
+console.log(location.href);
+console.log(location.pathname);
+console.log(location.host);
+console.log(location.origin);
+// location.reload()
+
+
+
