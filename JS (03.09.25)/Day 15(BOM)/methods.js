67d1ce8a0e1ab49545f97487e5f4cdb3132fb8b7
Raj Agrahari <raj.agr2002@gmail.com>
2025-09-28 20:44:58 +0530

    Adding

---
+++ JS (03.09.25)/Day 15(BOM)/methods.js
@@ -0,0 +1,15 @@
+// ! prompt
+// used for input by user
+
+let input = prompt("Enter tour crush name ");
+
+// ! alert
+
+alert("Your crush in messaging you");
+
+// ! confirm
+
+let elegibalForVote = confirm("Are ypu elegibal for vote ot not");
+console.log(elegibalForVote);
+
+
---
+++ JS (03.09.25)/Day 15(BOM)/proprties.js
@@ -0,0 +1,35 @@
+// ! screen
+
+console.log(screen);
+console.log(screen.height);
+console.log(screen.width);
+console.log(screen.orientation); // landscape or portrat
+
+
+//!  history
+history.go(2)// it will move forword two pages
+history.go(-2)//it will move back by two pages
+console.log();
+
+
+// ! navigator
+
+// console.log(navigator.onLine);
+// console.log(navigator.length);
+// console.log(navigator);
+
+
+//! location
+
+console.log(location);
+console.log(location.href);
+console.log(location.pathname);
+console.log(location.host);
+console.log(location.origin);
+console.log(location.protocol);
+
+// location.reload() // it will relode the webpage
+// location.reload() // it will assing a new m  the webpage
+
+
+
