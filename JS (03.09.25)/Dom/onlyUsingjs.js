f43085a8ef92ce6f32d5123e694a61b21d5cbc4e
Raj Agrahari <raj.agr2002@gmail.com>
2025-09-29 12:03:54 +0530

    Adding

---
+++ JS (03.09.25)/Dom/onlyUsingjs.js
@@ -0,0 +1,14 @@
+let body  = document.body;
+body.innerHTML  = `<h1>This is a heading </h1>`
+body.innerHTML  = `<h2>This is a heading </h2>`
+body.innerHTML  = `<ol>
+<li>potato</li>
+<li>tomoto</li>
+<li>cabbage</li>
+</ol>`
+
+body.innerHTML += `<ul>
+<li>coffee</li>
+<li>tea</li>
+<li>Juice pila do </li>
+</ul>`
