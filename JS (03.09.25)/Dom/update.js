b914fc76e655ab5b51d31ab7fed3b6e0627947ec
Raj Agrahari <raj.agr2002@gmail.com>
2025-09-29 11:52:06 +0530

    Adding

--- JS (03.09.25)/Dom/update.js
+++ JS (03.09.25)/Dom/update.js
@@ -23,5 +23,34 @@ console.log('===========================================');
 
 console.log(first.textContent);
 
+// ! update 
+
+
+// innerText
+// innerHTML
+// textContent
+
+
+let first = document.querySelector('#first')
+console.log(first);
+
+first.innerHTML = 'Hello , Akshay'
+first.innerText = 'Hello <i>Akshay</i>'
+first.textContentext = 'Hello <i>Akshay</i>'
+
+
+let first = document.querySelector('#first');
+console.log(first.innerText);
+console.log('===========================================');
+
+console.log(first.innerHTML);
+console.log('===========================================');
+
+console.log(first.textContent)
+
+
+
+
+
 
 
