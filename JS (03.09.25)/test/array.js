05a1861b8462855346cf6af650f25dc6f9b34dfc
Raj Agrahari <raj.agr2002@gmail.com>
2025-10-12 20:36:00 +0530

    Test in a class

---
+++ JS (03.09.25)/test/array.js
@@ -0,0 +1,32 @@
+const users = [
+  { id: 1, name: "Akshay", age: 22, city: "Delhi", isActive: true },
+  { id: 2, name: "Neha", age: 28, city: "Mumbai", isActive: false },
+  { id: 3, name: "Rohit", age: 25, city: "Pune", isActive: true },
+  { id: 4, name: "Sneha", age: 19, city: "Delhi", isActive: false },
+  { id: 5, name: "Karan", age: 30, city: "Bangalore", isActive: true }
+]
+
+let userName = users.map((item)=>{
+    return item.name
+})
+console.log(userName);
+
+let data = users.filter((item)=>{
+    if(item.isActive===true)
+        return item
+})
+console.log(data);
+
+
+let location = users.find((item)=>{
+    if(item.city==='Delhi')
+        return item
+})
+
+console.log(location);
+
+let age = users.reduce((acc,el)=>{
+    return acc+el.age
+},0)
+
+console.log(age);
